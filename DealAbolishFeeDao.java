cb8accdbdb5ce06120dbe9965353f5046e5a32ab
ZhaQW <38174708+ZhaQW@users.noreply.github.com>
2019-06-11 17:15:33 +0800

    Update DealAbolishFeeDao.java

--- DealAbolishFeeDao.java
+++ DealAbolishFeeDao.java
@@ -5,11 +5,11 @@ import com.watch.glamour.pojo.deal.DealAbolishFee;
 import java.util.List;
 
 /**
- *   测试中介交易取消费用说明
+ *   中介交易取消费用说明
  */
 public interface DealAbolishFeeDao {
     /**
-     * 测试查看中介交易取消费用说明
+     * 查看中介交易取消费用说明
      *
      * @return
      */

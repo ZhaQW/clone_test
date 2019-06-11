5c945a93da08b87dbb06964e98761b62e71c7ee2
zqw <2250296772@qq.com>
2019-06-11 16:35:24 +0800

    更新代码文件

---
+++ DealAbolishFeeDao.java
@@ -0,0 +1,17 @@
+package com.watch.glamour.dao.deal;
+
+import com.watch.glamour.pojo.deal.DealAbolishFee;
+
+import java.util.List;
+
+/**
+ * 中介交易取消费用说明
+ */
+public interface DealAbolishFeeDao {
+    /**
+     * 查看中介交易取消费用说明
+     *
+     * @return
+     */
+    List<DealAbolishFee> findAllRepairFee();
+}
